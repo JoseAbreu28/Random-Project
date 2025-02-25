17e3bed70a065821df1f9a19bc3998c6d8e3330a
Commander28 <50963613+JoseAbreu28@users.noreply.github.com>
2025-02-25 23:32:21 +0000

    Update cats_bypass.py

--- cats_bypass.py
+++ cats_bypass.py
@@ -13,7 +13,19 @@ def get_initial_cookies():
         "cats_wbt_session": input("Digite o valor de cats_wbt_session: "),
     }
 
-def send_request(auth_token, cookies,subject):
+disciplines = {
+    "AGK": {"docid": 475, "version": 77, "endPage": 378, "subject": 142},
+    "PoF": {"docid": 476, "version": 80, "endPage": 398, "subject": 148},
+    "OP": {"docid": 464, "version": 66, "endPage": 66, "subject": 147},
+    "NAV": {"docid": 477, "version": 81, "endPage": 327, "subject": 146},
+    "MET": {"docid": 466, "version": 68, "endPage": 249, "subject": 145},
+    "HPL": {"docid": 463, "version": 96, "endPage": 143, "subject": 144},
+    "FPP": {"docid": 479, "version": 83, "endPage": 168, "subject": 143},
+    "AL": {"docid": 467, "version": 69, "endPage": 307, "subject": 141},
+    "COM": {"docid": 461, "version": 62, "endPage": 99, "subject": 149},
+}
+
+def send_request(auth_token, cookies, subject):
     url = "https://wbt.catsaviation.com/3/includes/log_activity"
     headers = {"X-Requested-With": "XMLHttpRequest"}
     cookies["X-AUTH-TOKEN"] = auth_token
@@ -26,11 +38,11 @@ def send_request(auth_token, cookies,subject):
         print(f"Erro na requisição: {e}")
     return auth_token
 
-def send_get_docs_request(auth_token, cookies, docid, page):
+def send_get_docs_request(auth_token, cookies, docid, page, version, endPage):
     url = "https://wbt.catsaviation.com/3/docs/get"
     headers = {"X-Requested-With": "XMLHttpRequest"}
     cookies["X-AUTH-TOKEN"] = auth_token
-    data = {"type": "sg", "doc": docid, "version": "80", "page": page, "startPage": "0", "endPage": "398"}
+    data = {"type": "sg", "doc": docid, "version": version, "page": page, "startPage": "0", "endPage": endPage}
     try:
         requests.post(url, headers=headers, cookies=cookies, data=data, verify=False)
     except requests.exceptions.RequestException as e:
@@ -51,20 +63,34 @@ cookies = get_initial_cookies()
 
 # Token inicial
 auth_token = input("Digite o token inicial (X-AUTH-TOKEN): ")
-#verify on /docs/get the params version=80&page=10&startPage=0&endPage=398
-docid = int(input("Adicione o código da disciplina (docid POF=476, MET=466 , AGK=475 ,AL=467 , OP=464): "))
+
+# Menu de escolha de disciplina
+print("Escolha uma disciplina:")
+for key in disciplines:
+    print(f"{key}")
+discipline_choice = input("Digite o código da disciplina: ").strip().upper()
+
+if discipline_choice not in disciplines:
+    print("Disciplina inválida!")
+    exit()
+
+discipline_data = disciplines[discipline_choice]
+docid = discipline_data["docid"]
+version = discipline_data["version"]
+endPage = discipline_data["endPage"]
+subject = discipline_data["subject"]
+
 page = int(input("Digite a página inicial: "))
-subject = int(input("Digite a subject (POF=148 , MET=145, OP=147): "))
 time_alive = int(input("Digite o tempo de execução em minutos: ")) * 60
 start_time = time.time()
 
 while time.time() - start_time < time_alive:
-    auth_token = send_request(auth_token, cookies,subject)
-    send_get_docs_request(auth_token, cookies, docid, page)
+    auth_token = send_request(auth_token, cookies, subject)
+    send_get_docs_request(auth_token, cookies, docid, page, version, endPage)
     send_check_pagesubject_request(auth_token, cookies, docid, page)
     page += 1
     remaining_time = time_alive - (time.time() - start_time)
-    delay = random.randint(45, 210)
+    delay = random.randint(45, 300)
     print(f"Aguardando {delay} segundos para o próximo request...")
     time.sleep(delay)
 
